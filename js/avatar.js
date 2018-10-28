fdeb9315dc87aa843fde49b1e26d23e968042ba7
Vlada <33199915+VladaBelopolskaya@users.noreply.github.com>
2018-10-28 23:21:53 +0400

    Add files via upload

---
+++ js/avatar.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+    /**
+     * Установка имени и аватрки, по данным, которые пришли с сервера
+     * @param name имя юзера
+     * @param url аватарка юзера
+     */
+    function setLoginInfo(name, url) {
+        const userName = document.querySelector('#user');
+        const userPhoto = document.querySelector('#img');
+        userName.textContent = name;
+        userPhoto.src = url;
+    }
+
+    /**
+     * Очищение localStorage и переход на страницу авторизации
+     */
+    function logout() {
+        document.location.href = "index.html";
+        localStorage.removeItem('nameUser');
+        localStorage.removeItem('imgUser');
+    }
+
+    setLoginInfo(localStorage.getItem('nameUser'), localStorage.getItem('imgUser'));
+    const logoutButton = document.querySelector('#logout');
+    logoutButton.addEventListener('mouseup', logout);
+})();
---
+++ js/login.js
@@ -0,0 +1,86 @@
+'use strict';
+
+(function () {
+    const URL_SEND = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
+
+    /**
+     * Отправка email и password на сервер
+     */
+    function upload() {
+
+        removeError();
+
+        const xhr = new XMLHttpRequest();
+        const json = {
+            email: document.querySelector('#email').value,
+            password: document.querySelector('#password').value
+        }
+
+        xhr.responseType = 'json';
+        xhr.open('POST', URL_SEND);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(json));
+
+        xhr.onload = function () {
+            if (xhr.response.error) {
+                errorLogin();
+                errorEmail();
+            } else {
+                document.location.href = "avatar.html"
+                localStorage.setItem('nameUser', xhr.response.name);
+                localStorage.setItem('imgUser', xhr.response.photoUrl);
+            }
+        };
+        xhr.onerror = function () {
+            alert('Произошла ошибка соединения');
+        };
+
+        console.log(xhr.status);
+    }
+
+    /**
+     * Удаление блока с ошибкой
+     */
+    function removeError() {
+        const error = document.querySelector('.block__error');
+        if (error) {
+            error.remove();
+        }
+    }
+
+    /**
+     * Отображения блока с ошибкой
+     */
+    function errorLogin() {
+        const form = document.querySelector('#form');
+        const textError = document.createElement('p');
+        const password = document.querySelector('#password');
+        password.value = password.defaultValue;
+        textError.textContent = 'E-Mail or password is incorrect';
+        textError.classList.add('block__error');
+        form.insertBefore(textError, form.lastElementChild);
+    }
+
+    /**
+     * Выделение красным поля с почтой, если там есть данные
+     */
+    function errorEmail() {
+        const emailInput = document.querySelector('#email')
+        if (emailInput.value) {
+            emailInput.classList.add('block__input--error');
+        }
+    }
+
+    /**
+     * Удаление класса, выделяющего почту и border красным
+     */
+    function removeClass() {
+        const emailInput = document.querySelector('#email');
+        emailInput.classList.remove('block__input--error');
+    }
+
+    const buttonSubmit = document.querySelector('#login');
+    buttonSubmit.addEventListener('mouseup', upload);
+    const inputEmail = document.querySelector('#email');
+    inputEmail.addEventListener('focus', removeClass);
+})();
